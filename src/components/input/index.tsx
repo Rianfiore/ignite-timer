1b44142dcc44a141095a2e4ea059294fbae8411b
Rian Fiore <74125852+Rianfiore@users.noreply.github.com>
2022-08-08 19:46:14 -0300

    render with modifier condition

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,9 +1,23 @@
+import { Icon } from "components"
+
 import "./styles.scss"
 import { InputProps } from "./types"
 
 export const Input = ({type, placeholder, modifier = false} : InputProps) => {
 
-  return (
-    <input className="input" placeholder={placeholder} type={type} />
-  )
+
+  const renderInput = () => {
+    
+    const render = modifier ? (
+     <>
+        <Icon icon="minus" alt="minus icon"/>
+        <input className="input" placeholder={placeholder} type={type} />
+        <Icon icon="plus" alt="plus icon"/>
+     </>
+    ) : <input className="input" placeholder={placeholder} type={type} />
+    
+    return render;
+  }
+
+  return renderInput()
 }
