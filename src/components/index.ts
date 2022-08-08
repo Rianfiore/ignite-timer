f3d60216586fdb21363386736ab3cc229e522660
Rianfiore <rianfiore@gmail.com>
2022-08-08 11:28:33 -0300

    controlling all components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { Paragraph } from './paragraph';
+export { Flex } from './flex';
+export { Icon } from './icon';
+export { Grid } from "./grid";
+export { NavBar } from "./nav-bar";
+export { Main } from "./main";
