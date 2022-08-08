20571c53500abc6a006f4d59ba663940c42a825d
Rianfiore <rianfiore@gmail.com>
2022-08-08 10:56:32 -0300

    create icon component

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,7 +1,14 @@
+import { iconData } from "icons/data";
 import { IconProps } from "./types";
+import "./styles.scss"
 
-export const Icon = ({} : IconProps) => {
+export const Icon = ({ icon, alt, size = "normal"}: IconProps) => {
+
+  const findIcon = iconData.find(object => icon === object.name)
+
   return (
-    <div></div>
+    <figure className={`icon icon--${size}`}>
+      <img src={findIcon?.src} alt={alt} />
+    </figure>
   )
 }
