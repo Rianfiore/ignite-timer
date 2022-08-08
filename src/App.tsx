71d9e5ab95b3d8488418afdd96ba218499061b30
Rianfiore <rianfiore@gmail.com>
2022-08-08 10:30:48 -0300

    adding Home and Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { NavBar } from "components";
+import { Home } from "pages";
 import "./App.scss"
 
 export const App = () => {
   return (
     <div className="App">
-      <header className="header">
-        
-      </header>
+      <NavBar />
+      <Home />
     </div>
   );
 }
