c21caefa673fb8356dc050b901c270d063b524bc
Rian Fiore <74125852+Rianfiore@users.noreply.github.com>
2022-08-08 14:39:54 -0300

    add input in home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,11 @@
-import { Flex, Main, Paragraph} from "components"
+import { Flex, Main, Paragraph, Input} from "components"
 
 export const Home = () => {
   return (
     <Main>
       <Flex gap="0.5%">
         <Paragraph>Vou trabalhar em</Paragraph>
+        <Input type="text" placeholder="Dê um nome para o seu projeto"></Input>
         <Paragraph>durante</Paragraph>
         <Paragraph>minutos.</Paragraph>
       </Flex>
