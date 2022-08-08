a3342196d794d03b9e6e1d52ac222a45d73f64b3
Rianfiore <rianfiore@gmail.com>
2022-08-08 11:27:53 -0300

    create home component

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import { Flex, Main, Paragraph} from "components"
+
+export const Home = () => {
+  return (
+    <Main>
+      <Flex gap="0.5%">
+        <Paragraph>Vou trabalhar em</Paragraph>
+        <Paragraph>durante</Paragraph>
+        <Paragraph>minutos.</Paragraph>
+      </Flex>
+    </Main>
+  )
+}
