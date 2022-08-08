b6691a1689626e1097ee06a7d2d823d8d8d36ebe
Rianfiore <rianfiore@gmail.com>
2022-08-08 11:26:57 -0300

    add icons

---
+++ src/icons/data.ts
@@ -0,0 +1,19 @@
+import { logo, clock, historic } from "icons"
+
+export const iconData = [
+  {
+    id: 0,
+    name: "logo",
+    src: logo
+  },
+  {
+    id: 1,
+    name: "clock",
+    src: clock
+  },
+  {
+    id: 2,
+    name: "historic",
+    src: historic
+  }
+]
---
+++ src/icons/index.ts
@@ -0,0 +1,5 @@
+import logo from "./logo.svg"
+import clock from "./clock.svg"
+import historic from "./historic.svg"
+
+export { logo, clock, historic }
