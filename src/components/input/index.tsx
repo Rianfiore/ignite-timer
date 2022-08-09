6b5997d915981f6eb31d078acd6d683b3ef1ab3f
Rian Fiore <74125852+Rianfiore@users.noreply.github.com>
2022-08-09 20:43:08 -0300

    -add functionalities for input component;

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,17 +1,27 @@
 import { Icon } from "components"
-
+import { useState, useEffect } from "react"
 import "./styles.scss"
 import { InputProps } from "./types"
 
-export const Input = ({type, placeholder, modifier = false} : InputProps) => {
+export const Input = ({type, placeholder = "", modifier = false, modifierMin = 0, modifierMax = Infinity} : InputProps) => {
+  const [input, setInput] = useState<number>(Number(placeholder))
+  const size = placeholder.length * 1.2;
+
+  const addSubValue = (operator : string) => {
+      if (input >= modifierMax) return
+      operator === "add" && setInput(input + 1)
+      
+      if (input <= modifierMin) return
+      operator === "sub" && setInput(input - 1)
+  }
 
   return (
     modifier ? (
       <>
-         <Icon icon="minus" alt="minus icon"/>
-         <input className={`input input--${type}`} placeholder={placeholder} type={type} />
-         <Icon icon="plus" alt="plus icon"/>
+         <Icon icon="minus" alt="minus icon" onHover onClick={() => addSubValue("sub")}/>
+         <input className={`input input--${type}`} placeholder={input.toString()} value={input} type={type} size={size} onChange={e => setInput(Number(e.target.value))}/>
+         <Icon icon="plus" alt="plus icon" onHover onClick={() => addSubValue("add")}/>
       </>
-     ) : <input className={`input input--${type}`} placeholder={placeholder} type={type} />
+     ) : <input className={`input input--${type}`} placeholder={placeholder} type={type} size={size} />
   )
 }
