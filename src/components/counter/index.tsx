47197a3d4c02850b9f75e90bc827b65e7d455dd6
annaestherrr <annaesther_15@hotmail.com>
2022-08-09 17:29:24 -0300

    - delete old tags;
    - add functionality to the count component;

--- src/components/counter/index.tsx
+++ src/components/counter/index.tsx
@@ -1,11 +1,37 @@
+import { useState } from 'react';
+
+
+const SECOND_DEFAULT = 300;
+
 export const Counter = () => {
-  return (
-    <div>
-      <span>0</span>
-      <span>0</span>
-      <span>:</span>
-      <span>0</span>
-      <span>0</span>
-    </div>
+
+const [seconds, setSeconds] = useState(SECOND_DEFAULT);
+
+const secondsToTime = (secs: number) => {
+  //uma hora tem 3600 segundos
+  const divisorMinute = secs % 3600;
+
+  const minutes = Math.floor( divisorMinute / 60);
+  const seconds = Math.ceil( divisorMinute % 60);
+
+  //retornar uma string e concatenar minutos por sengundos
+  //a função padStart vai manter os digitos a esquerda, o "2" é quantidade de digitos, o "0" o número que vai retornar na tela
+  return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+;}
+
+
+const startTimer = () => {
+  setInterval(() => {
+    setSeconds((previousSeconds) => {
+      return previousSeconds - 1;
+    })
+  }, 1000)
+}
+
+return (
+  <div>
+    <h1>{secondsToTime(seconds)}</h1>
+    <button onClick={() => startTimer()}>start</button>
+  </div>
   )
 }
